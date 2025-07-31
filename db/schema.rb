aa25871dbdcee327f645eb8082158bd1d2bfefcb
Jamie <jamiecleare2525@hotmail.com>
2025-07-31 10:19:34 +0100

    Actively looking EOI complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_21_133642) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_30_143307) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
@@ -109,13 +109,22 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_21_133642) do
 
   create_table "placement_subjects", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", null: false
-    t.string "code", null: false
+    t.string "code"
     t.enum "phase", enum_type: "phase"
     t.uuid "parent_subject_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["code"], name: "index_placement_subjects_on_code", unique: true
     t.index ["parent_subject_id"], name: "index_placement_subjects_on_parent_subject_id"
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.string "session_id", null: false
+    t.text "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+    t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
   create_table "user_memberships", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
